6a825d408455ce345877fa19019c15be6e280b7c
Mike <mike@none.com>
2017-08-22 19:10:03 -0500

    improve background pizza movement. no need for 200 when absolute positioning 3 rows of 8 covers most screens.

--- views/js/main.js
+++ views/js/main.js
@@ -490,9 +490,10 @@ function updatePositions() {
     window.performance.mark("mark_start_frame");
 
     var items = document.querySelectorAll('.mover');
+    var scroll = (document.body.scrollTop / 1250);
     for (var i = 0; i < items.length; i++) {
-        var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-        items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+        //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+        items[i].style.transform = "translate(" + (100 * Math.sin(scroll + (i % 5))) + "px, 0)";
     }
 
     // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -512,13 +513,14 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function () {
     var cols = 8;
     var s = 256;
-    for (var i = 0; i < 200; i++) {
+    for (var i = 0; i < 40; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
         elem.src = "images/pizza.png";
         elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
+        elem.style.left = ((i % cols) * s) + "px";
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
         document.querySelector("#movingPizzas1").appendChild(elem);
     }
